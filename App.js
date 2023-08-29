6562b9be3a031402839c77ca7e20cc1dbae32890
VictorPcode <116080710+VictorPcode@users.noreply.github.com>
2023-08-29 00:05:50 -0400

    trabajando en el inicio muestras para categoria y productos

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 import 'react-native-gesture-handler';
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import NavigationDrawer from "./src/navigation/NavigationDrawer";
-import {AuthProvider} from './src/context/AuthContext'
+import { AuthProvider } from './src/context/AuthContext';
+
+// Importa la fuente personalizada
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  // Carga la fuente personalizada
+  const [loaded] = useFonts({
+    'Phudu-Regular': require('./assets/fonts/Phudu-Regular.ttf'), // Asegúrate de que la ruta sea correcta
+  });
+
+  if (!loaded) {
+    // Si la fuente no se ha cargado, puedes tomar una acción alternativa
+    // Puedes mostrar una fuente predeterminada o algún otro comportamiento
+    return null;
+  }
+
+  // Configura la fuente predeterminada para todos los componentes Text
+  Text.defaultProps = Text.defaultProps || {};
+  Text.defaultProps.style = { fontFamily: 'Phudu-Regular', color: '#fff', fontSize: '14px' };
+
   return (
     <NavigationContainer>
       <AuthProvider>
-      <NavigationDrawer/>
+        <NavigationDrawer />
       </AuthProvider>
     </NavigationContainer>
   );
@@ -22,5 +40,9 @@ const styles = StyleSheet.create({
     color: "white",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontFamily: 'Phudu-Regular', // El nombre de la fuente que configuraste
+    fontSize: 24,
   },
 });
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,125 @@
-import {Text, Button } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import React from 'react';
+import React, { useState } from 'react';
+import { Text, View, Image, StyleSheet, SectionList, Button, TouchableOpacity } from 'react-native'; // Importa TouchableOpacity
+import SegmentedControlTab from 'react-native-segmented-control-tab';
+import Modal from 'react-native-modal';
+
+const dummyData = [
+  {
+    title: 'Zapatos',
+    data: [
+      {
+        id: '1',
+        image: 'https://i.pinimg.com/564x/a6/f5/e9/a6f5e9aabebdedec1f4e397a5dd20c84.jpg',
+        name: 'Zapatos Nike',
+        price: '100',
+      },
+      {
+        id: '2',
+        image: 'https://i.pinimg.com/564x/ce/6d/c7/ce6dc7341c4bb3a2356faac89e896c6a.jpg',
+        name: 'Shoes',
+        price: '300',
+      },
+    ],
+  },
+  {
+    title: 'Smarthphone',
+    data: [
+      {
+        id: '1',
+        image: 'https://www.notebookcheck.net/fileadmin/Notebooks/News/_nc3/Samsung_Galaxy_S23_Ultra_concept_7.jpg',
+        name: 'Samsung s23',
+        price: '100',
+      },
+      {
+        id: '2',
+        image: 'https://th.bing.com/th/id/OIP.TCyQbDsA8m5XEDZ9BJJsfgHaFQ?pid=ImgDet&rs=1',
+        name: 'Iphone 13',
+        price: '300',
+      },
+    ],
+  },
+  // Agrega más categorías y datos ficticios según sea necesario
+];
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: '#000000',
+  },
+  productImage: {
+    width: '100%',
+    height: 150,
+  },
+  centerPosition: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  categoryButton: {
+    backgroundColor: 'orange',
+    padding: 10,
+    borderRadius: 5,
+    margin: 5,
+    alignItems: 'center',
+  },
+  categoryButtonText: {
+    color: 'white',
+  },
+});
 
 export default function Home(props) {
-  const {navigation} = props;
-  const goToSettings = () =>{
-    navigation.navigate('Configuraciones')
-  }
-  // console.log(props)
+  const { navigation } = props;
+  const [isCategoryModalVisible, setCategoryModalVisible] = useState(false);
+
+  const goToSettings = () => {
+    navigation.navigate('Configuraciones');
+  };
+
+  const toggleCategoryModal = () => {
+    setCategoryModalVisible(!isCategoryModalVisible);
+  };
 
   return (
-    <SafeAreaView >
-      <Text>Inicio</Text>
-    </SafeAreaView>
-  )
-
-  }
+    <View style={styles.wrapper}>
+      <Button  variant={'outlined'} color={'orange'}  title="CATEGORIAS" onPress={toggleCategoryModal} />
+      <Modal isVisible={isCategoryModalVisible}>
+        <View>
+          <TouchableOpacity onPress={toggleCategoryModal}>
+            <Text>Close</Text>
+          </TouchableOpacity>
+          {dummyData.map((category, index) => (
+            <TouchableOpacity
+              key={index}
+              onPress={() => {
+                setSelectedCategoryIndex(index);
+                toggleCategoryModal();
+              }}
+              style={styles.categoryButton}
+            >
+              <Text style={styles.categoryButtonText}>{category.title}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </Modal>
+      <SectionList
+        style={styles.wrapper}
+        stickySectionHeadersEnabled={false}
+        sections={dummyData.map(category => ({
+          title: category.title,
+          data: category.data,
+        }))}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Image source={{ uri: item.image }} style={styles.productImage} />
+            <Text>{item.name}</Text>
+            <Text>{item.price}</Text>
+            <Button  variant={'outlined'} color={'orange'} className={styles.centerPosition} title="Ver Detalles" onPress={goToSettings} />
+          </View>
+        )}
+        renderSectionHeader={({ section }) => (
+          <Text>{section.title}</Text>
+        )}
+      />
+    </View>
+  );
+}
